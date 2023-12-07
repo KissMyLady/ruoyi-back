e1601bf36706037b5e0a53bae850656a6920aa63
kissmylady <kissmyladys@gmail.com>
2023-12-07 14:08:26 +0800

    update(): 移除部门查询分页

---
+++ ruoyi-admin/src/test/java/com/ruoyi/test_main_v1/Test_magic_v1.java
@@ -0,0 +1,16 @@
+package com.ruoyi.test_main_v1;
+
+/**
+ * @Author: kissmylady
+ * @Date: 2023/12/6 16:42
+ * @Describe:
+ **/
+public class Test_magic_v1 {
+
+    public void test_1(){
+        String a1 = "aa";
+        String a2 = "aa";
+
+        System.out.println(a1.equals(a2));
+    }
+}
